040964c6f5827fffeb837b0e8166fa783ea54dd4
Adir2000-ops <69550146+Adir2000-ops@users.noreply.github.com>
2021-03-17 21:10:37 -0300

    oo

---
+++ libs/Conecxao.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class Conecxao{
+    private $username = 'root';
+    private $password = 'root';
+    private $banco    = 'tela-principal';
+    private $host     = 'localhost';
+    private $conn;    
+}
+
